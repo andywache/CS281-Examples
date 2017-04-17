df81821d6f083404d3bec09b4a0b70d3072b0305
andywache <andyeazer@gmail.com>
2017-04-17 10:15:33 +0800

    class 2

--- Lecture01/example01/Program.cs
+++ Lecture01/example01/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace example01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        /// <summary>
+        /// ｝
+        /// </summary>
+        /// <param name="args"></param>
+        //private static void Main(string[] args)
+        //{
+        //    string str = "100.3";
+        //    float number = 0.0f;
+        //    float.TryParse(str, out number);
+        //}
+        Console.Write("使用者名稱:");
+private string userName = Console.ReadLine();
+        Console.Write("請輸入密碼");
+private string userPassword = Console.ReadLine();
+        Console.WriteLine("{0} 您好，登入成功", userName);
     }
 }
