c4c47f76c8102418133e19fe5e5c1c33e9414696
andywache <andyeazer@gmail.com>
2017-04-18 08:33:38 +0800

    class L2 add 0417

--- Lecture01/example01/Program.cs
+++ Lecture01/example01/Program.cs
@@ -8,20 +8,33 @@ namespace example01
 {
     internal class Program
     {
-        /// <summary>
-        /// ｝
-        /// </summary>
-        /// <param name="args"></param>
-        //private static void Main(string[] args)
-        //{
-        //    string str = "100.3";
-        //    float number = 0.0f;
-        //    float.TryParse(str, out number);
-        //}
-        Console.Write("使用者名稱:");
-private string userName = Console.ReadLine();
-        Console.Write("請輸入密碼");
-private string userPassword = Console.ReadLine();
-        Console.WriteLine("{0} 您好，登入成功", userName);
+        private static void Main(string[] args)
+        {
+            int? FirstNum = null;
+            int? SecNum = null;
+            try
+            {
+                Console.Write("請輸入第一個號碼:");
+                FirstNum = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Write("第一個號碼錯誤:");
+                return;
+            }
+
+            try
+            {
+                Console.Write("請輸入第二個號碼:");
+                SecNum = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Write("第二個號碼錯誤:");
+                return;
+            }
+
+            Console.WriteLine(FirstNum + SecNum);
+        }
     }
 }
