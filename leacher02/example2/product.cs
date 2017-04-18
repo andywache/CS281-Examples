99c2519686d78f8de50feceba7f087704688a1e5
andywache <andyeazer@gmail.com>
2017-04-18 09:11:04 +0800

    add L2 file

---
+++ leacher02/example2/product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example2
+{
+    internal class product
+    {
+        public string Name;
+        public int Price;
+    }
+}
