dcb6c80022df9a745853c3db2d1b5a58ebcba017
andywache <andyeazer@gmail.com>
2017-04-18 10:00:54 +0800

    add  L2 homework

---
+++ lectuer02/example04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            long ms = 0;
+            Console.Write("請輸入毫秒：");
+            try
+            {
+                ms = long.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("資料輸入錯誤!!");
+                return;
+            }
+
+            long sec = ms / 1000 % 60;        //計算秒數
+            long min = ms / 1000 / 60 % 60;         //計算分鐘
+            long hr = ms / 1000 / 60 / 60;          //計算小時
+
+            Console.Write("轉換後：" + ms + "毫秒 = " + hr + "小時 ," + min + "分鐘 ," + sec + "秒鐘\n");
+        }
+    }
+}
